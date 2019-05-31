abd6c8b89e3e4a12d611dcbeb4543b0ef3395900
stevenjchang <stevenjchang@gmail.com>
2019-05-31 10:05:32 -0700

    flatten sample data

--- src/tennisData/data.js
+++ src/tennisData/data.js
@@ -10,70 +10,135 @@ const newTime = (month, day, hour=0, min=0) => {
   return new Date(2019, (month - 1), day, hour, min)
 }
 
-export const tournament = {  //
+export const tournament = {
   custom_id: '2019_french',
   name: '2019 French Open',
-  round1: [
-    {}
-  ],
-  round2: [
+  year: 2019,
+  open: 'french',
+  matches: [
     {
+      round: 2,
       dateTime: newTime(5, 29, 4),
       player1: {
         id,
         fullName: 'K Nishikori',
-        rank: 7,
+        ranking: 7,
         winner: true,
         result: [4, 6, 6, 6],
       },
       player2: {
         id,
         fullName: 'J Tsonga',
-        rank: null,
+        ranking: null,
         winner: false,
         result: [6, 4, 4, 4],
       },
     },
     {
-      dateTime: new Date(2019, 5, 30, 11),
+      round: 2,
+      dateTime: newTime(5, 29, 4),
       winner: 'player1',
       player1: {
         id,
         fullName: 'D Thiem',
-        rank: 4,
+        ranking: 4,
         winner: true,
         result: [6, 6, 6, 7]
       },
       player2: {
         id,
         fullName: 'A Bublik',
-        rank: null,
+        ranking: null,
         winner: false,
         result: [3, 7, 3, 5]
       },
     },
     {
-      dateTime: new Date(2019, 5, 30, 11),
+      round: 2,
+      dateTime: newTime(5, 29, 4),
       player1: {
         id,
         fullName: 'P Cuevas',
-        rank: null,
+        ranking: null,
         winner: true,
         result: [7, 6, 2],
       },
       player2: {
         id,
         fullName: 'K Edmonds',
-        rank: 28,
+        ranking: 28,
         winner: false,
         result: [6, 3, 1],
       },
     },
+    {
+      round: 3,
+      dateTime: newTime(5, 31, 4),
+      player1: {
+        id,
+        fullName: 'K Nishikori',
+        ranking: 7,
+        winner: null,
+        result: [],
+      },
+      player2: {
+        id,
+        fullName: 'L Djere',
+        ranking: 31,
+        winner: null,
+        result: [],
+      }
+    },
+    {
+      round: 3,
+      dateTime: newTime(5, 31, 4),
+      player1: {
+        id,
+        fullName: 'P Carreno Busta',
+        ranking: null,
+      },
+      player2: {
+        id,
+        fullName: 'B Paire',
+        ranking: null,
+      },
+    },
   ],
 
+//  -----------------  SAMPLE  -----------------
+  sample: [
+    {
+      rounds: 0,
+      dateTime: newTime(5, 31, 4),
+      player1: {
+        id,
+        fullName: '',
+        ranking: null,
+        winner: null,
+        result: [],
+      },
+      player2: {
+        id,
+        fullName: '',
+        ranking: null,
+        winner: null,
+        result: [],
+      },
+    },
+  ]
 
-// ----------------- ROUND 3 -----------------
-  round3: [
+}
+
+const result = tournament.matches.filter(match => {
+  // console.log('match ==>', match);
+  if (match.player1.fullName === 'K Nishikori') {
+    console.log('found ==>', match);
+  }
+})
+
+// -----------------  FIELDS TO ADD  -----------------
+
+/*
     {
       // dateTime: new Date(2019, 5-1, 31, 11, 45),
       dateTime: newTime(5, 31, 4),
@@ -87,53 +152,12 @@ export const tournament = {  //
       player1: {
         id,
         fullName: 'K Nishikori',
-        rank: 7,
+        ranking: 7,
       },
       player2: {
         id,
         fullName: 'L Djere',
-        rank: 31,
+        ranking: 31,
       }
     },
-    {
-      dateTime: newTime(5, 31, 4),
-      player1: {
-        id,
-        fullName: 'P Carreno Busta',
-        rank: null,
-      },
-      player2: {
-        id,
-        fullName: 'B Paire',
-        rank: null,
-      },
-    },
-
-    {
-      dateTime: newTime(5, 31, 4),
-      player1: {
-        id,
-        fullName: '',
-        rank: null,
-        winner: null,
-        result: [],
-      },
-      player2: {
-        id,
-        fullName: '',
-        rank: null,
-        winner: null,
-        result: [],
-      },
-    },
-
-  ]
-
-}
-
-const result = tournament.round3.filter(match => {
-  // console.log('match ==>', match);
-  if (match.player1.fullName === 'K Nishikori') {
-    console.log('found ==>', match);
-  }
-})
+*/
