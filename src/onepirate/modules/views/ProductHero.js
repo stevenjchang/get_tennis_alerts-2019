b84a9436a7d07c57c9ddd04e84ce3c2635bf2901
Steve <stevenjchang@gmail.com>
2019-06-17 16:00:58 -0700

    Change theme (#15)
    * import theme
    
    * add static files
    
    * fix - path for _utils/compose
    
    * [path] fix - import static in public folder
    
    * [text] update main text
    
    * [text] update text
    
    * [font] reduce font-size
    
    * [nit] update text
    
    * [ui] add beta text/positioning
    
    * [ui] change Hero background
    
    * [ui] change title maxWidth

---
+++ src/onepirate/modules/views/ProductHero.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '../components/Button';
+import Typography from '../components/Typography';
+import ProductHeroLayout from './ProductHeroLayout';
+
+const backgroundImage =
+  // 'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400&q=80';
+  '/static/themes/onepirate/tennis-575578-unsplash.jpg';
+
+const styles = theme => ({
+  background: {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundColor: '#7fc7d9', // Average color of the background image.
+    backgroundPosition: 'center',
+  },
+  button: {
+    minWidth: 200,
+  },
+  h5: {
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(4),
+    [theme.breakpoints.up('sm')]: {
+      marginTop: theme.spacing(4),
+    },
+  },
+  more: {
+    marginTop: theme.spacing(2),
+  },
+  title1: {
+    maxWidth: '625px',
+  }
+});
+
+function ProductHero(props) {
+  const { classes } = props;
+
+  return (
+    <ProductHeroLayout backgroundClassName={classes.background}>
+      {/* Increase the network loading priority of the background image. */}
+      <img style={{ display: 'none' }} src={backgroundImage} alt="" />
+      <Typography color="inherit" align="center" variant="h3" marked="center" className={classes.title1}>
+        {/* Upgrade your Sundays */}
+        Get FREE email alerts when the next match happens.
+      
+        <br />
+        <br />
+        <Typography color="inherit" align="center" variant="h5" marked="center">
+          Follow your favorite players.
+          <br />
+          <br />
+        </Typography>
+      </Typography>
+
+      <Typography color="inherit" align="center" variant="h7" className={classes.h5}>
+        {/* Enjoy secret offers up to -70% off the best luxury hotels every Sunday. */}
+        Simple and easy. Never miss another match!
+      </Typography>
+      <Button
+        color="secondary"
+        variant="contained"
+        size="large"
+        className={classes.button}
+        component="a"
+        href="/premium-themes/onepirate/sign-up/"
+      >
+        {/* Register */}
+        Choose Players
+      </Button>
+      <Typography variant="h8" color="inherit" className={classes.more}>
+        {/* Discover the experience */}
+        {/* No Spam! This a hobby project, made for tennis fans. */}
+      </Typography>
+    </ProductHeroLayout>
+  );
+}
+
+ProductHero.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ProductHero);
