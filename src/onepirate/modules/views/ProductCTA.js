5c8c3a1ab6f9fb6fe5eba50abb9366b1c34e3537
stevenjchang <stevenjchang@gmail.com>
2019-06-18 16:24:13 -0700

    [ui][text] add 'no spam' text

--- src/onepirate/modules/views/ProductCTA.js
+++ src/onepirate/modules/views/ProductCTA.js
@@ -19,50 +19,55 @@ const styles = theme => ({
   root: {
     marginTop: theme.spacing(10),
     marginBottom: 0,
-    display: 'flex',
+    display: "flex"
   },
   cardWrapper: {
-    zIndex: 1,
+    zIndex: 1
   },
   card: {
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: '#e4e9ed',
-    padding: theme.spacing(8, 3),
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "#e4e9ed",
+    padding: theme.spacing(8, 3)
   },
   cardContent: {
-    maxWidth: 400,
+    maxWidth: 400
   },
   textField: {
-    width: '100%',
+    width: "100%",
     marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(4),
+    marginBottom: theme.spacing(4)
   },
   button: {
-    width: '100%',
-    backgroundColor: '#ff3366',
+    width: "100%",
+    backgroundColor: "#ff3366"
   },
   imagesWrapper: {
-    position: 'relative',
+    position: "relative"
   },
   imageDots: {
-    position: 'absolute',
+    position: "absolute",
     top: -67,
     left: -67,
     right: 0,
     bottom: 0,
-    width: '100%',
-    background: 'url(/static/onepirate/productCTAImageDots.png)',
+    width: "100%",
+    background: "url(/static/onepirate/productCTAImageDots.png)"
   },
   image: {
-    position: 'absolute',
+    position: "absolute",
     top: -28,
     left: -28,
     right: 0,
     bottom: 0,
-    width: '100%',
-    maxWidth: 600,
+    width: "100%",
+    maxWidth: 600
   },
+  noSpamText: {
+    marginTop: theme.spacing(1),
+    display: "flex",
+    justifyContent: "center"
+  }
 });
 
 const anchorStyles = {
@@ -162,6 +167,9 @@ class ProductCTA extends React.Component {
                     {/* Keep me updated */}
                     Get Email Alerts!
                   </Button>
+                  <Typography variant="h8" className={classes.noSpamText}>
+                    No Spam. This is a hobby project.
+                  </Typography>
                 </form>
               </div>
             </Grid>
