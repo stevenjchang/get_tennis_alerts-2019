b0caad59b1570cd883d02a865407c9ac24080d66
stevenjchang <stevenjchang@gmail.com>
2019-06-17 17:27:09 -0700

    [nit] refactor import statement

--- src/components/PlayerSearchBar.js
+++ src/components/PlayerSearchBar.js
@@ -2,22 +2,19 @@ import React from 'react';
 import Select from 'react-select';
 import { playerNames } from '../tennisData/playerNames';
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla1', label: 'Vanilla2' },
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry1', label: 'Strawberry2' },
-  { value: 'vanilla', label: 'Vanilla' }
-]
 
-export default () => (
-  <Select
-    // defaultValue={[playerNames[0], playerNames[2]]}
-    isMulti
-    name="colors"
-    options={playerNames}
-    className="basic-multi-select"
-    classNamePrefix="select"
-  />
+const PlayerSearchBar = () => (
+  <>
+    <h4>Type your favorite players:</h4>
+    <Select
+      // defaultValue={[playerNames[0], playerNames[2]]}
+      isMulti
+      name="colors"
+      options={playerNames}
+      className="basic-multi-select"
+      classNamePrefix="select"
+    />
+  </>
 );
+
+export default PlayerSearchBar;
