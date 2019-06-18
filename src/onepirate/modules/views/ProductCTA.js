8cf98e37714511d3c39ce7aa1091c495572ca60a
stevenjchang <stevenjchang@gmail.com>
2019-06-18 14:55:41 -0700

    [ux] set checkbox default to true

--- src/onepirate/modules/views/ProductCTA.js
+++ src/onepirate/modules/views/ProductCTA.js
@@ -68,6 +68,8 @@ const styles = theme => ({
 class ProductCTA extends React.Component {
   state = {
     open: false,
+    checkboxA: true,
+    checkboxB: true,
   };
 
   handleSubmit = event => {
@@ -121,11 +123,13 @@ class ProductCTA extends React.Component {
                 <div style={{marginTop: '70px'}}>
                   <Checkbox 
                     name="checkboxA"
+                    checked={this.state.checkboxA}
                     onChange={this.handleCheckbox}
                   />
                   <span>1 day before match</span>
                   <Checkbox
                     name="checkboxB"
+                    checked={this.state.checkboxB}
                     onChange={this.handleCheckbox}
                   />
                   <span>1 hour before match</span>
