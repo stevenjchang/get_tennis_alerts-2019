7873a927c8f9f809f613c5a156c1afb3de5485f1
Steve <stevenjchang@gmail.com>
2019-06-19 16:00:14 -0700

    [ui][text] add Announcement of when the draw will be available (#36)
    * [ui][text][temp] add annoucement of draw release
    
    * [docs] add css color
    
    * [ui][css] reposition annoucement div

--- src/onepirate/modules/views/ProductHero.js
+++ src/onepirate/modules/views/ProductHero.js
@@ -4,6 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Button from '../components/Button';
 import Typography from '../components/Typography';
 import ProductHeroLayout from './ProductHeroLayout';
+import Paper from '@material-ui/core/Paper';
 
 const backgroundImage =
   // 'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400&q=80';
@@ -12,24 +13,30 @@ const backgroundImage =
 const styles = theme => ({
   background: {
     backgroundImage: `url(${backgroundImage})`,
-    backgroundColor: '#7fc7d9', // Average color of the background image.
-    backgroundPosition: 'center',
+    backgroundColor: "#7fc7d9", // Average color of the background image.
+    backgroundPosition: "center"
   },
   button: {
-    minWidth: 200,
+    minWidth: 200
   },
   h5: {
     marginBottom: theme.spacing(4),
     marginTop: theme.spacing(4),
-    [theme.breakpoints.up('sm')]: {
-      marginTop: theme.spacing(4),
-    },
+    [theme.breakpoints.up("sm")]: {
+      marginTop: theme.spacing(4)
+    }
   },
   more: {
-    marginTop: theme.spacing(2),
+    marginTop: theme.spacing(2)
   },
   title1: {
-    maxWidth: '625px',
+    maxWidth: "625px"
+  },
+  paper: {
+    padding: theme.spacing(2, 6),
+    backgroundColor: "#ffff7e",
+    // marginTop: "-4.5%",
+    marginBottom: "30px"
   }
 });
 
@@ -39,21 +46,39 @@ function ProductHero(props) {
   return (
     <ProductHeroLayout backgroundClassName={classes.background}>
       {/* Increase the network loading priority of the background image. */}
-      <img style={{ display: 'none' }} src={backgroundImage} alt="" />
-      <Typography color="inherit" align="center" variant="h3" marked="center" className={classes.title1}>
+      <img style={{ display: "none" }} src={backgroundImage} alt="" />
+      <Paper className={classes.paper}>
+        The draw for Wimbledon will be available starting 06/28
+      </Paper>
+      <Typography
+        color="inherit"
+        align="center"
+        variant="h3"
+        marked="center"
+        className={classes.title1}
+      >
         {/* Upgrade your Sundays */}
         Get FREE email alerts when the next match happens.
-      
         <br />
         <br />
-        <Typography color="inherit" align="center" variant="h5" marked="center">
+        <Typography
+          color="inherit"
+          align="center"
+          variant="h5"
+          marked="center"
+        >
           Follow your favorite players.
           <br />
           <br />
         </Typography>
       </Typography>
 
-      <Typography color="inherit" align="center" variant="h7" className={classes.h5}>
+      <Typography
+        color="inherit"
+        align="center"
+        variant="h7"
+        className={classes.h5}
+      >
         {/* Enjoy secret offers up to -70% off the best luxury hotels every Sunday. */}
         Simple and easy. Never miss another match!
       </Typography>
