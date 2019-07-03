d6703ae35853a6b25316ca1913fd5e39f15b83be
stevenjchang <stevenjchang@gmail.com>
2019-07-03 16:58:42 -0700

    [nit] prettify

--- src/components/Email/mockMailingListData.js
+++ src/components/Email/mockMailingListData.js
@@ -1,64 +1,61 @@
 require("dotenv").config();
 
-const generateEmail = (id) => (`${process.env.EMAIL_USERNAME}+user${id}${process.env.EMAIL_DOMAIN}`);
+const generateEmail = id =>
+  `${process.env.EMAIL_USERNAME}+user${id}${process.env.EMAIL_DOMAIN}`;
 
 export const mockMailingList = [
-         {
-           id: 1,
-           email: generateEmail(1),
-           selectedPlayers: [
-             "Novak Djokovic",
-             "Rafael Nadal",
-             "Dominic Thiem"
-           ],
-           checkboxA: true,
-           checkboxB: true,
-           timeZoneLocationString: "America/Los_Angeles",
-           timeZoneOffset: "-0700"
-         },
-         {
-           id: 2,
-           email: generateEmail(2),
-           selectedPlayers: ["Stanislas Wawrinka"],
-           checkboxA: false,
-           checkboxB: false,
-           timeZoneLocationString: "Asia/Taipei",
-           timeZoneOffset: "+0800"
-         }
-         //   {
-         //   id:,
-         //   email:,
-         //   selectedPlayers: [
+  {
+    id: 1,
+    email: generateEmail(1),
+    selectedPlayers: ["Novak Djokovic", "Rafael Nadal", "Dominic Thiem"],
+    checkboxA: true,
+    checkboxB: true,
+    timeZoneLocationString: "America/Los_Angeles",
+    timeZoneOffset: "-0700"
+  },
+  {
+    id: 2,
+    email: generateEmail(2),
+    selectedPlayers: ["Stanislas Wawrinka"],
+    checkboxA: false,
+    checkboxB: false,
+    timeZoneLocationString: "Asia/Taipei",
+    timeZoneOffset: "+0800"
+  }
+  //   {
+  //   id:,
+  //   email:,
+  //   selectedPlayers: [
 
-         //   ],
-         //   checkboxA:,
-         //   checkboxB:,
-         // },
-         //   {
-         //   id:,
-         //   email:,
-         //   selectedPlayers: [
+  //   ],
+  //   checkboxA:,
+  //   checkboxB:,
+  // },
+  //   {
+  //   id:,
+  //   email:,
+  //   selectedPlayers: [
 
-         //   ],
-         //   checkboxA:,
-         //   checkboxB:,
-         // },
-         //   {
-         //   id:,
-         //   email:,
-         //   selectedPlayers: [
+  //   ],
+  //   checkboxA:,
+  //   checkboxB:,
+  // },
+  //   {
+  //   id:,
+  //   email:,
+  //   selectedPlayers: [
 
-         //   ],
-         //   checkboxA:,
-         //   checkboxB:,
-         // },
-         //   {
-         //   id:,
-         //   email:,
-         //   selectedPlayers: [
+  //   ],
+  //   checkboxA:,
+  //   checkboxB:,
+  // },
+  //   {
+  //   id:,
+  //   email:,
+  //   selectedPlayers: [
 
-         //   ],
-         //   checkboxA:,
-         //   checkboxB:,
-         // },
-       ];
+  //   ],
+  //   checkboxA:,
+  //   checkboxB:,
+  // },
+];
