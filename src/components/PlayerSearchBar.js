be13a38878386fb14e8cf8e5cbe4aa8615047c17
stevenjchang <stevenjchang@gmail.com>
2019-07-06 17:06:54 -0700

    nit - remove unused code

--- src/components/PlayerSearchBar.js
+++ src/components/PlayerSearchBar.js
@@ -3,21 +3,17 @@ import Select from 'react-select';
 import { playerNames } from '../tennisData/playerNames';
 import connectTennis from '../containers/connectTennis';
 
-const values = [
-  {value: 'val', label: 'label'}
-]
-
 
 const PlayerSearchBar = ({ selectedPlayers,setSelectedPlayers }) => {
 
   useEffect(() => {
     localStorage.setItem("selectedPlayers", JSON.stringify(selectedPlayers));
     return;
-  })
-
+  });
+  
   const handleOnChange = (selectedPlayers) => {
     setSelectedPlayers(selectedPlayers);
-  }
+  };
 
   return (
     <>
