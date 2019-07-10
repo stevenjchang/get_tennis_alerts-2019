f2059727307165677eda4edcb9eb00ab1ae00e15
stevenjchang <stevenjchang@gmail.com>
2019-07-09 23:01:20 -0700

    [ui] change toggle label size

--- src/components/ToggleOptions.js
+++ src/components/ToggleOptions.js
@@ -3,12 +3,13 @@ import connectTennis from "../containers/connectTennis";
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
+import Typography from "@material-ui/core/Typography";
 
 
 const ToggleOptions = ({ showHistory, toggleShowHistory }) => {
   return (
     <>
-      <FormGroup row style={{ justifyContent: 'center' }}>
+      <FormGroup row style={{ justifyContent: "flex-end" }}>
         <FormControlLabel
           control={
             <Switch
@@ -17,7 +18,12 @@ const ToggleOptions = ({ showHistory, toggleShowHistory }) => {
               value="checkedA"
             />
           }
-          label="Show Past Matches"
+          // label="Show Past Matches"
+          label={
+            <Typography variant="headline" component="p">
+              show history
+            </Typography>
+          }
           labelPlacement="end"
           labelStyle={{ fontSize: "0.75rem" }}
         />
