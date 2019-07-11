e3ee39c14c195802c12cd2b571df43af96589339
stevenjchang <stevenjchang@gmail.com>
2019-07-11 12:40:33 -0700

    [ui] change toggle position

--- src/components/ToggleOptions.js
+++ src/components/ToggleOptions.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import connectTennis from "../containers/connectTennis";
-import FormGroup from '@material-ui/core/FormGroup';
+import { makeStyles } from "@material-ui/core/styles";
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
 import Switch from '@material-ui/core/Switch';
 import Typography from "@material-ui/core/Typography";
+import connectTennis from "../containers/connectTennis";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    justifyContent: "flex-end",
+    marginRight: '-0.7rem',
+  }
+}));
 
 
 const ToggleOptions = ({ showHistory, toggleShowHistory }) => {
+  const classes = useStyles();
+
   return (
     <>
-      <FormGroup row style={{ justifyContent: "flex-end" }}>
+      <FormGroup row className={classes.root}>
         <FormControlLabel
           control={
             <Switch
@@ -18,14 +29,12 @@ const ToggleOptions = ({ showHistory, toggleShowHistory }) => {
               value="checkedA"
             />
           }
-          // label="Show Past Matches"
           label={
             <Typography variant="headline" component="p">
               show history
             </Typography>
           }
           labelPlacement="end"
-          labelStyle={{ fontSize: "0.75rem" }}
         />
       </FormGroup>
     </>
