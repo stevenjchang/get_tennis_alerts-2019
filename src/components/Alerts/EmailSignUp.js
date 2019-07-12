197c6231583116c80e268d229ec642ba3010a5e3
Steve <stevenjchang@gmail.com>
2019-07-11 20:01:01 -0700

    Fix signup button and timezone payload (#55)
    * Email sign up working (without timezone)
    
    * fix timeZone not in redux store
    
    * remove unused reducer - timeZone

--- src/components/Alerts/EmailSignUp.js
+++ src/components/Alerts/EmailSignUp.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import connectTennis from "../../containers/connectTennis";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "../../onepirate/modules/components/Button";
+import Snackbar from "../../onepirate/modules/components/Snackbar";
 import Typography from "../../onepirate/modules/components/Typography";
 import TextField from "../../onepirate/modules/components/TextField";
 import theme from "../../onepirate/modules/theme";
+
+import { _sendEmailSignUp } from './utils/_sendEmailSignUp';
 
 const useStyles = makeStyles({
   textField: {
@@ -23,16 +26,29 @@ const useStyles = makeStyles({
   }
 });
 
-const EmailSignUp = () => {
+const EmailSignUp = (props) => {
   const classes = useStyles();
   const [email, setEmail] = useState('');
+  const [showSuccessMessage, toggleSuccessMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    _sendEmailSignUp(email, props)
+      .then((res) => {
+        console.log('Sign Up complete ==>', res);
+        toggleSuccessMessage(true);
+        setEmail('')
+      })
+      .catch((err) => { console.log('Error in EmailSignUp ==>', err) })
+  }
 
   return (
-    <div style={{
-      maxWidth: '75%',
-      margin: '60px auto',
-
-    }}>
+    <div
+      style={{
+        maxWidth: "75%",
+        margin: "60px auto"
+      }}
+    >
       <TextField
         id="filled-email-input"
         label="Email"
@@ -50,13 +66,18 @@ const EmailSignUp = () => {
         color="primary"
         variant="contained"
         className={classes.button}
+        onClick={handleSubmit}
       >
-        {/* Keep me updated */}
         Get Email Alerts!
       </Button>
       <Typography variant="h8" className={classes.noSpamText}>
         No Spam. This is a hobby project.
       </Typography>
+      <Snackbar
+        open={showSuccessMessage}
+        onClose={() => toggleSuccessMessage(false)}
+        message="Thank you. You are now subscribed."
+      />
     </div>
   );
 };
