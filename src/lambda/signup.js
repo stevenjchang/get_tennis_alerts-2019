250256ceb6232b03f5028270d70e37726c86394d
stevenjchang <stevenjchang@gmail.com>
2019-08-26 14:08:03 -0700

    comment out everything

--- src/lambda/signup.js
+++ src/lambda/signup.js
@@ -1,46 +1,46 @@
-const MongoClient = require("mongodb").MongoClient;
-require("dotenv").config();
+// const MongoClient = require("mongodb").MongoClient;
+// require("dotenv").config();
 
-const DB_URI = process.env.MONGODB_URI;
-const DB_NAME = process.env.MONGODB_DBNAME;
-let cachedDb = null;
+// const DB_URI = process.env.MONGODB_URI;
+// const DB_NAME = process.env.MONGODB_DBNAME;
+// let cachedDb = null;
 
 // this pattern comes from:
 // https://docs.atlas.mongodb.com/best-practices-connecting-to-aws-lambda/
 
-function connectToDatabase(uri) {
-  console.log("=> connect to database");
-  if (cachedDb) {
-    console.log("=> using cached database instance");
-    return Promise.resolve(cachedDb);
-  }
-  return MongoClient.connect(uri).then(db => {
-    cachedDb = db;
-    return cachedDb;
-  });
-}
+// function connectToDatabase(uri) {
+//   console.log("=> connect to database");
+//   if (cachedDb) {
+//     console.log("=> using cached database instance");
+//     return Promise.resolve(cachedDb);
+//   }
+//   return MongoClient.connect(uri).then(db => {
+//     cachedDb = db;
+//     return cachedDb;
+//   });
+// }
 
-function addEmailToMailingList(db, dbName, payload) {
-  return db
-    .db(dbName)
-    .collection("mailingList")
-    .insertOne({
-      selectedPlayers: payload.selectedPlayers,
-      email: payload.email,
-      checkboxA: payload.checkboxA,
-      checkboxB: payload.checkboxB,
-      timeZoneLocationString: payload.timeZoneLocationString,
-      timeZoneOffset: payload.timeZoneOffset,
-      created_at: new Date()
-    })
-    .then(data => {
-      return data;
-    })
-    .catch(err => {
-      console.log("=> an error occurred: ", err);
-      return { statusCode: 500, body: "error" };
-    });
-}
+// function addEmailToMailingList(db, dbName, payload) {
+//   return db
+//     .db(dbName)
+//     .collection("mailingList")
+//     .insertOne({
+//       selectedPlayers: payload.selectedPlayers,
+//       email: payload.email,
+//       checkboxA: payload.checkboxA,
+//       checkboxB: payload.checkboxB,
+//       timeZoneLocationString: payload.timeZoneLocationString,
+//       timeZoneOffset: payload.timeZoneOffset,
+//       created_at: new Date()
+//     })
+//     .then(data => {
+//       return data;
+//     })
+//     .catch(err => {
+//       console.log("=> an error occurred: ", err);
+//       return { statusCode: 500, body: "error" };
+//     });
+// }
 
 /* how to get http request payload:
 
@@ -56,12 +56,12 @@ post requests -
 */
 
 exports.handler = function(event, context, callback) {
-  context.callbackWaitsForEmptyEventLoop = false;
-  console.log(
-    "=> queryStringParameters: ",
-    JSON.stringify(event.queryStringParameters)
-  );
-  console.log("=> event.body: ", event.body);
+  // context.callbackWaitsForEmptyEventLoop = false;
+  // console.log(
+  //   "=> queryStringParameters: ",
+  //   JSON.stringify(event.queryStringParameters)
+  // );
+  // console.log("=> event.body: ", event.body);
 
   // const payload = JSON.parse(event.body);
 
